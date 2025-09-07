6baaf717ec65faa6d300469edc67213ffaa53749
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2025-09-08 01:40:35 +0300

    feat: complete findLongestIncreasingSubsequence task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -492,8 +492,25 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  return nums
+    .reduce(
+      ([sequence, result], curr, i) => {
+        if (!i) {
+          return [1, 1];
+        }
+
+        if (curr > nums[i - 1]) {
+          const newSequence = sequence + 1;
+
+          return [newSequence, newSequence > result ? newSequence : result];
+        }
+
+        return [1, result];
+      },
+      [0, 0]
+    )
+    .at(1);
 }
 
 /**
