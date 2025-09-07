800e9bf3ba6ccfa2d9caa2bd7684b7fce36d71e1
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2025-09-07 23:19:07 +0300

    feat: complete getAverage task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -132,8 +132,12 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return (
+    Number(
+      (arr.reduce((acc, curr) => acc + curr, 0) / arr.length).toFixed(2)
+    ) || 0
+  );
 }
 
 /**
